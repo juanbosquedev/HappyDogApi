e4c301a5148522f3daabdfe5293a9d090a406af6
juanchos85 <86373520+juanchos85@users.noreply.github.com>
2024-03-08 08:15:16 -0300

    next

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ app.get('/', (req, res) => {
   res.send('Hola yo');
 });
 
+app.get('/next', (req, res) => {
+  res.send('Hola ya');
+});
+
 // Iniciar el servidor
 app.listen(8000, () => {
   console.log(`Server listening on port: ${8000}`);
