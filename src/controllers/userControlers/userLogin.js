3579a12553a2e405f7927af72c78467eecb42d8e
juanchos85 <86373520+juanchos85@users.noreply.github.com>
2024-03-24 17:31:12 -0300

    normalizing data

--- src/controllers/userControlers/userLogin.js
+++ src/controllers/userControlers/userLogin.js
@@ -1,18 +1,23 @@
 const { Registers } = require("../../database/database");
+const jwt = require("jsonwebtoken");
 
 module.exports = async (req, res) => {
   try {
     const { email, password } = req.body;
     const userFromDB = await Registers.findOne({ where: { email, password } });
+
     if (!userFromDB) {
-      return res
-        .status(400)
-        .json({ error: "Credenciales inválidas. Inténtalo de nuevo." });
+      return res.status(400).json({ error: "Credenciales inválidas. Inténtalo de nuevo." });
     }
-    if (userFromDB) {
-      await userFromDB.update({ logged: true });
-      return res.status(200).send(userFromDB);
-    }
+
+    // Generar un token JWT con una expiración de 60 minutos
+    const token = jwt.sign({ id: userFromDB.id }, process.env.JWT_SECRET, { expiresIn: '60m' });
+
+    // Marcar al usuario como autenticado (opcional)
+    await userFromDB.update({ logged: true });
+
+    // Devolver el token JWT junto con los datos del usuario
+    return res.status(200).json({ userFromDB, token });
   } catch (error) {
     console.error(error);
     res.status(500).send("Error interno del servidor");
