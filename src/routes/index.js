9098f2abb98f25d2262d5a7c68b40938f58f0429
juanchos85 <86373520+juanchos85@users.noreply.github.com>
2024-03-24 17:29:10 -0300

    jwt authentication

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,26 +1,28 @@
-const { Router } = require("express");
-const {
-  userLogin,
-  userRegister,
-  userLogout,
-  allDogs,
-  newDog,
-  dogDelete,
-  dogUpdate,
-} = require("../controllers");
+const { Router } = require("express");const {
+    userLogin,
+    userRegister,
+    userLogout,
+    allDogs,
+    newDog,
+    dogDelete,
+    dogUpdate,
+  } = require("../controllers");
+  
+const authenticate = require("../middleware/authenticate"); 
 
 const router = Router();
-//users
-router.post("/userLogin/:id", userLogin);
-router.post("/userLogout/:id", userLogout)
+
+// user
+router.post("/userLogin", userLogin);
+router.post("/userLogout", userLogout);
 router.post("/userRegister", userRegister);
 
-//dogs
+// dogs
 router.get("/allDogs", allDogs);
-router.post("/newDog", newDog );
-router.put("/updateDog/:id", dogUpdate);
-router.delete("/dogDelete/:id", dogDelete);
-
-
+router.post("/newDog", newDog);       
+router.put("/updateDog/:id", dogUpdate); 
+router.delete("/dogDelete/:id", dogDelete); 
 
 module.exports = router;
+
+
