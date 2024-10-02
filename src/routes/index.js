877b21bca964b7bb40b94c485e3035e75130efdc
juan <juan@shaper.us>
2024-10-02 15:30:28 -0300

    feat(api): route index

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,28 +1,10 @@
-const { Router } = require("express");const {
-    userLogin,
-    userRegister,
-    userLogout,
-    allDogs,
-    newDog,
-    dogDelete,
-    dogUpdate,
-  } = require("../controllers");
-  
-const authenticate = require("../middleware/authenticate"); 
+const { Router } = require("express");
+const userRoutes = require("./userRoutes/index");
+const dogRoutes = require("./dogRoutes/index");
 
 const router = Router();
 
-// user
-router.post("/userLogin", userLogin);
-router.post("/userLogout", userLogout);
-router.post("/userRegister", userRegister);
-
-// dogs
-router.get("/allDogs", allDogs);
-router.post("/newDog", newDog);       
-router.put("/updateDog/:id", dogUpdate); 
-router.delete("/dogDelete/:id", dogDelete); 
+router.use("/user", userRoutes);
+router.use("/dog", dogRoutes);
 
 module.exports = router;
-
-
