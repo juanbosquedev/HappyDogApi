b212d817d2039cc54defa4eb919da1037e01550b
juan <juan@shaper.us>
2024-10-02 10:33:03 -0300

    chore: new model name, config, deleted host prop

--- src/models/dogs/DogsRecord.js
+++ src/models/dogs/DogsRecord.js
@@ -1,48 +1,45 @@
-module.exports = (sequelize, DataTypes) => {
-  const Dogs = sequelize.define(
-    "Dogs",
-    {
-      id: {
-        type: DataTypes.INTEGER,
-        autoIncrement: true,
-        primaryKey: true,
-        allowNull: false,
-      },
-      hostage_id: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-      },
-      name: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      city: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      height: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      weight: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      image: {
-        type: DataTypes.STRING,
-      },
-      personality: {
-        type: DataTypes.STRING,
-        allowNull: true,
-      },
-      life_span: {
-        type: DataTypes.STRING,
-        allowNull: true,
-      },
+const { DataTypes } = require("sequelize");
+const { sequelize } = require("../../database/connection-db.js");
+
+const DogsRecord = sequelize.define(
+  "dog",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+      allowNull: false,
     },
-    {
-      timestamps: false,
-    }
-  );
-  return Dogs;
-};
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    city: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    height: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    weight: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    image: {
+      type: DataTypes.STRING,
+    },
+    personality: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    life_span: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+  },
+  {
+    timestamps: false,
+  }
+);
+module.exports = DogsRecord;
