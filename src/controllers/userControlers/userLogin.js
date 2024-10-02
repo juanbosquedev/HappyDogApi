0a60c49f46eb63fb619062696106436550a50c0d
Juan Bosque <86373520+juanbosquedev@users.noreply.github.com>
2024-10-01 21:34:37 -0300

    Update userLogin.js

--- src/controllers/userControlers/userLogin.js
+++ src/controllers/userControlers/userLogin.js
@@ -4,21 +4,22 @@ const jwt = require("jsonwebtoken");
 module.exports = async (req, res) => {
   try {
     const { email, password } = req.body;
-    const userFromDB = await Registers.findOne({ where: { email, password } });
+    // const userFromDB = await Registers.findOne({ where: { email, password } });
+const userFromDB = await Registers.findAll();
+    return res.status(200).json( userFromDB);
+    // if (!userFromDB) {
+    //   return res
+    //     .status(400)
+    //     .json({ error: "Credenciales inválidas. Inténtalo de nuevo." });
+    // }
 
-    if (!userFromDB) {
-      return res
-        .status(400)
-        .json({ error: "Credenciales inválidas. Inténtalo de nuevo." });
-    }
+    // const token = jwt.sign({ id: userFromDB.id }, process.env.JWT_SECRET, {
+    //   expiresIn: "60m",
+    // });
 
-    const token = jwt.sign({ id: userFromDB.id }, process.env.JWT_SECRET, {
-      expiresIn: "60m",
-    });
+    // await userFromDB.update({ logged: true });
 
-    await userFromDB.update({ logged: true });
-
-    return res.status(200).json({ userFromDB, token });
+    // return res.status(200).json({ userFromDB, token });
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: "Error interno del servidor" });
