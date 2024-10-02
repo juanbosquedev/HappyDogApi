33f61b7af07f7da15ff5c1fe4682cf67b15d362e
juan <juan@shaper.us>
2024-10-02 11:25:59 -0300

    fix: new models import and fill 1° time db

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
 require("dotenv").config();
+const { sequelize } = require("./src/database/connection-db.js");
 const server = require("./src/server");
-const { sequelize, Register, Dog } = require("./src/database/database");
 const { PORT } = process.env;
 
+require("./src/models/index.js");
+require("./src/models/association/associations.js");
+const { fillDbDogs, fillDbUsers } = require("./src/utils/index.js");
 
 (async function main() {
   try {
-    await sequelize.sync({force:false});
-    console.log("Login, conection has been established successfully.") ;
+    await sequelize.sync({ force: false });
+    await fillDbUsers();
+    await fillDbDogs();
+    console.log("Login, conection has been established successfully.");
     server.listen(PORT);
     console.log(`Server lingening on: ${PORT}`);
   } catch (error) {
     console.error(`Unable to connect to the database: ${error}`);
   }
-})()
-
-
+})();
